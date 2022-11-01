ff3296d892d5cb838d94e7e6318ae65f6fee0df2
zulhisyam03 <zul0342@gmail.com>
2022-11-01 19:04:09 +0800

    remove yajra

--- config/app.php
+++ config/app.php
@@ -195,7 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Yajra\DataTables\DataTablesServiceProvider::class
+        // Yajra\DataTables\DataTablesServiceProvider::class
 
     ],
 
